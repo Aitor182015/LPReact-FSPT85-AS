7e8a8f1c7c5b968434e1c41fbe77b08a6f40cf4e
Aitor <aitorsantos90@gmail.com>
2024-11-13 20:30:05 +0000

    practica terminada

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">A Warm Welcome</h1>
+          <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo autem saepe blanditiis ipsa nostrum nesciunt aspernatur nobis quis. Commodi, qui maxime facilis nulla voluptates aut voluptate quod consequatur atque provident!</p>
+          <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+        </div>
+      </div>
+      
+)
+}
+
+export default Jumbotron
